164b723b307dfc5d69af60fca2a44b15afe38f94
khadkaabishek <abishekkhadka36@gmail.com>
2025-04-15 21:13:48 +0545

    Day2 done

--- Day2/script.js
+++ Day2/script.js
@@ -19,3 +19,14 @@ fs.readFile('./data.txt','utf-8',(err , result)=>{   // Asynchrounous File Readi
         console.log(result);
     }
 })
+
+
+
+fs.appendFileSync('./data.txt',new Date().getDate(). toLocaleString()); // Appending content from last ? adds the exact date of today 
+
+
+fs.cpSync("./data.txt","./copy.txt"); // copies file 
+
+fs.unlinkSync('./copy.txt');  //deletes the file 
+
+console.log(fs.statSync('./data.txt').isFile());
