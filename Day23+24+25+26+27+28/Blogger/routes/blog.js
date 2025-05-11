45c21e845e54970a27e227e98027dd6da9697f3b
khadkaabishek <abishekkhadka36@gmail.com>
2025-05-11 23:49:07 +0545

    improveed more

---
+++ Day23+24+25+26+27+28/Blogger/routes/blog.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express();
+const User = require("../models/user");
+const multer = require("multer");
+const Blog = require("./../models/blog");
+const Comment = require("./../models/interaction");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/coverImages");
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.get("/add-new", (req, res) => {
+  return res.render("addBlog", {
+    loggedInUser: req.user,
+    user: req.user,
+  });
+});
+router.get("/our-blogs", async (req, res) => {
+  const ourBlog = await Blog.find({}).populate(
+    "createdBy",
+    "full_Name profileImageUrl"
+  );
+  console.log(ourBlog);
+  const allUser = await User.find({});
+
+  console.log(allUser);
+  return res.render("ourBlog", {
+    loggedInUser: req.user,
+    blogs: ourBlog,
+    user: allUser,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const clickedBlog = await Blog.findById({ _id: req.params.id });
+    // const interaction = await Comment.find({});
+    const owner = await Blog.findById(req.params.id).populate(
+      "createdBy",
+      "full_Name profileImageUrl"
+    );
+    console.log(owner);
+    if (!clickedBlog || !owner) {
+      return res.status(404).send("Blog or User not found");
+    }
+
+    return res.render("clickedBlog", {
+      loggedInUser: req.user,
+      blog: clickedBlog,
+      user: owner,
+    });
+  } catch (error) {
+    console.error("Error loading blog/user:", error);
+    res.status(500).send("Server error");
+  }
+});
+
+router.post("/:id", async (req, res) => {
+  try {
+    const clickedBlog = await Blog.findById({ _id: req.params.id });
+    const owner = await Blog.findById(req.params.id).populate(
+      "createdBy",
+      "full_Name profileImageUrl"
+    );
+    const comment = await Comment.create({
+      comment: req.comment,
+    });
+    return res.render("clickedBlog", {
+      loggedInUser: req.user,
+      blog: clickedBlog,
+      user: owner,
+    });
+  } catch (error) {
+    console.error("Error loading blog/user:", error);
+    res.status(500).send("Server error");
+  }
+});
+
+router.post("/add-new", upload.single("coverImage"), async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const blog = await Blog.create({
+    title: body.title,
+    body: body.body,
+    coverImageUrl: `/coverimages/${req.file.filename}`,
+    createdBy: req.user._id,
+  });
+  return res.redirect("/");
+});
+
+module.exports = router;
